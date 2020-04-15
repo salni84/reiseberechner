e18e4be9b5cc36ac3e720de36d6e5ab426cc406f
tchsaro1 <roberto.salerno@swissscom.com>
2020-04-15 10:26:52 +0200

    update2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SbbPageComponent} from "./sbb-page/sbb-page.component";
+import {Reisen} from "./reisen/reisen.component";
+
+const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: Reisen
+  },
+  {
+    path: 'sbb',
+    component: SbbPageComponent
+  },
 
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Reisen} from "./reisen/reisen.component";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { ChartComponent } from './chart/chart.component';
+import { NgApexchartsModule } from "ng-apexcharts";
+import { NavbarComponent } from './navbar/navbar.component';
+import { SbbPageComponent } from './sbb-page/sbb-page.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule} from "@ng-bootstrap/ng-bootstrap";
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Reisen,
+    ChartComponent,
+    NavbarComponent,
+    SbbPageComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    FormsModule,
+    NgApexchartsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule
   ],
+
+
   providers: [],
   bootstrap: [AppComponent]
 })
